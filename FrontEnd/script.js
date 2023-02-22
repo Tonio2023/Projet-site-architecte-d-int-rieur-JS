ba406e8d58dfe8dcd6ee6b155140af211840e875
Tonio2023 <beaudouxantoine@yahoo.fr>
2023-02-22 22:01:38 +0100

    premier commit test

---
+++ FrontEnd/script.js
@@ -0,0 +1,13 @@
+fetch('http://localhost:5678/api/works')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {console.log(data);
+
+    const elementfigure = document.createElement('p')
+    elementfigure.textContent = "hello"
+
+    document.querySelector('.gallery').appendChild(elementfigure)
+
+
+  });

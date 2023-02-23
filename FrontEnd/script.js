ccb04085772f78a2234b6db42723de8d6e07d23e
Tonio2023 <beaudouxantoine@yahoo.fr>
2023-02-23 15:37:01 +0100

    fetch essai

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,15 +2,31 @@ fetch('http://localhost:5678/api/works')
   .then(response => {
     return response.json();
   })
-  .then(data => {console.log(data);
+  .then(data => {
+    console.log(data);
 
-    const elementfigure = document.createElement('p')
-    elementfigure.textContent = "hello"
+    const firstElement = data[0];
+    const imgUrl = firstElement.imageUrl;
+    const captionText = 'Abat-jour Tahina';
 
-    document.querySelector('.gallery').appendChild(elementfigure)
+    // Créer un nouvel élément <figure>
+    const figure = document.createElement('figure');
 
+    // Créer un nouvel élément <img> et <figcaption>
+    const img = document.createElement('img');
+    img.src = imgUrl;
+    img.alt = 'Image';
+    const figcaption = document.createElement('figcaption');
+    figcaption.textContent = captionText;
+
+    // Insérer l'image et le figcaption dans la figure
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+
+    // Sélectionner la <div> avec la classe ".gallery"
+    const gallery = document.querySelector('.gallery');
+
+    // Insérer la figure dans la <div>
+    gallery.appendChild(figure);
 
   });
-
-
-  fetchldmdmdmdmd
